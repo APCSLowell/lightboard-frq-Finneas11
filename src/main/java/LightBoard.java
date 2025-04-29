df975fcf4ab4ce744d19df00aac4d494bc15e7b9
Finneas11 <filoftus@s.sfusd.edu>
2025-04-29 14:15:27 -0700

    Update LightBoard.java

--- src/main/java/LightBoard.java
+++ src/main/java/LightBoard.java
@@ -10,12 +10,12 @@ public class LightBoard
    */
   public LightBoard(int numRows, int numCols)
   {
- lights = new Boolean [numRows] [numCols];
+ lights = new boolean [numRows] [numCols];
 
     for ( int r = 0; r < numRows; r++ ) {
       for ( int c = 0; c < numCols; c++ ) {
           double rnd = Math.random(); 
-            lights[r][c] = rnd > 0.4;
+            lights[r][c] = rnd < 0.4;
       }
     }
       
